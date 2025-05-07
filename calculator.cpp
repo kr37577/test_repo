b16a555666ee9524ba8762da1724f88bacf63717
kr37577 <jiatengl127@gmail.com>
2025-05-07 16:14:13 +0900

    commit2

---
+++ calculator.cpp
@@ -0,0 +1,10 @@
+// calculator.cpp
+#include "calculator.hpp"
+
+int Calculator::add(int a, int b) {
+    return a + b; // Implementation for addition
+}
+
+int Calculator::subtract(int a, int b) {
+    return a - b; // Original subtraction
+}
