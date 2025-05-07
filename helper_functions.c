54af08b8602662a3703e3e0f0a41b2b171f05468
kr37577 <jiatengl127@gmail.com>
2025-05-07 16:15:02 +0900

    commit 4

---
+++ helper_functions.c
@@ -0,0 +1,12 @@
+// helper_functions.c
+#include "helper_functions.h"
+#include <stdio.h>
+
+void print_hello() {
+    printf("Hello from C helper!\n");
+}
+
+// This function will have its line number changed later
+int multiply_by_two(int x) {
+    return x * 2;
+}
