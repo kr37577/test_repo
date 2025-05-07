912876b2d138a31a6b98c64acf425681d9c25150
kr37577 <jiatengl127@gmail.com>
2025-05-07 21:48:56 +0900

    Added subtract function, modified add, updated main

--- calculator.cpp
+++ calculator.cpp
@@ -1,16 +1,33 @@
 #include <iostream>
 
+// Modified function
 int add(int a, int b) {
+    std::cout << "Adding " << a << " and " << b << std::endl; // Added a call
     return a + b;
+}
+
+// New function
+int subtract(int a, int b) {
+    return a - b;
 }
 
 int main() {
     int x = 10;
     int y = 5;
+    int z = 2; // Added assignment
     int sum = add(x, y);
+    int difference = subtract(x, y); // Added call
+
     std::cout << "Sum: " << sum << std::endl;
+    std::cout << "Difference: " << difference << std::endl; // Added call
+
     if (sum > 10) {
         std::cout << "Sum is greater than 10." << std::endl;
+        if (difference > 0) { // Added condition
+            std::cout << "Difference is positive." << std::endl;
+        }
+    } else {
+        std::cout << "Sum is not greater than 10." << std::endl;
     }
     return 0;
 }
