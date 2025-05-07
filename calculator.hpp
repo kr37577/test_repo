b9d4c579c69f2c07729c9b892e848e6f620528e2
kr37577 <jiatengl127@gmail.com>
2025-05-07 16:13:20 +0900

    first commit

---
+++ calculator.hpp
@@ -0,0 +1,11 @@
+// calculator.hpp
+#ifndef CALCULATOR_HPP
+#define CALCULATOR_HPP
+
+class Calculator {
+public:
+    int add(int a, int b);
+    int subtract(int a, int b);
+};
+
+#endif // CALCULATOR_HPP
